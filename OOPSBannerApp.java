000a83cae0407eef17b21203718d2cca8a1ec1f7
ys8896-cmd <ys8896@srmist.edu.in>
2026-02-22 13:21:04 +0530

    UC5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,16 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-		String[] words = new String[7];
+		String[] words = {
 
-        words[0] = String.join(" ", "   ***   ", "   ***   ", "****** ", "***** ");
-		words[1] = String.join(" ", " **   ** ", " **   ** ", "*  **", " **      ");
-		words[2] = String.join(" ", "**     **", "**     **", "*  *  ", "**       ");
-		words[3] = String.join(" ", "**     **", "**     **", "*** ", "  *****  ");
-		words[4] = String.join(" ", "**     **", "**     **", "*   ", "      ** ");
-		words[5] = String.join(" ", " **   ** ", " **   ** ", "*   ", " **   **");
-		words[6] = String.join(" ", "   ***   ", "   ***   ", "*   ", "  *****  ");
+        String.join(" ", "   ***   ", "   ***   ", "****** ", "***** "),
+		String.join(" ", " **   ** ", " **   ** ", "*  **", " **      "),
+		String.join(" ", "**     **", "**     **", "*  *  ", "**       "),
+		String.join(" ", "**     **", "**     **", "*** ", "  *****  "),
+		String.join(" ", "**     **", "**     **", "*   ", "      ** "),
+		String.join(" ", " **   ** ", " **   ** ", "*   ", " **   **"),
+		String.join(" ", "   ***   ", "   ***   ", "*   ", "  *****  ")
+		};
 		
 		for (String word : words){
 			System.out.println(word);
